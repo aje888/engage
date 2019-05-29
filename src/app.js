527cfca14bff09527a8818e65322d5a19c811cf3
Andrew Edwards <andrew.edwards@capgemini.com>
2019-05-29 09:34:37 +0100

    Increased payload limit to 100mb

--- src/app.js
+++ src/app.js
@@ -27,8 +27,8 @@ app.configure(configuration());
 app.use(cors());
 app.use(helmet());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 //app.use('/', express.static(app.get('public')));
