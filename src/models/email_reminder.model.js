e13fdd23f6b641653caf130308fed69d5e0885b3
HarvinderDhami <harvinder.dhami@baehub.io>
2019-06-04 12:13:38 +0100

    Email is now required

--- src/models/email_reminder.model.js
+++ src/models/email_reminder.model.js
@@ -1,49 +1,44 @@
-const NeDB = require('nedb');
-const path = require('path');
-const db = require('mongoose');
-
-
-
+const NeDB = require("nedb");
+const path = require("path");
+const db = require("mongoose");
+const validators = require("mongoose-validators");
 
 module.exports = {
-  createModel: function (app) {
-    const dbPath = app.get('nedb');
+  createModel: function(app) {
+    const dbPath = app.get("nedb");
     const Model = new NeDB({
-      filename: path.join(dbPath, 'email_reminder.db'),
+      filename: path.join(dbPath, "email_reminder.db"),
       autoload: true
     });
 
     return Model;
   },
-  schema: db.model('email-reminder', new db.Schema({
-      Name: {
-        type: "String",
-        required: [true, 'Email Reminder must have nName.'],
-        description: "The users name."
+  schema: db.model(
+    "email-reminder",
+    new db.Schema(
+      {
+        Email: {
+          type: String,
+          validate: validators.isEmail({
+            message: "Not a valid e-mail."
+          }), //'Valid Capgemini email address is required.'],
+          required: [true, "E-mail address is required."],
+          description: "The users e-mail."
+        },
+
+        URL: {
+          type: String,
+          validate: validators.isURL({
+            protocols: ["http", "https"],
+            require_tld: true,
+            require_protocol: true,
+            message: "Not a valid URL!"
+          }),
+          required: [true, "Activity URL is required."],
+          description: "The activity url."
+        }
       },
-	  
-	  Email: {
-        type: String,
-		validate: {
-			validator: function(v) {
-				return /@capgemini.com/.test(v);
-			},
-			message: props => `${props.value} is not a valid Capgemini email address!`
-		},
-        required: [true, 'Valid Capgemini email address is required.'],
-        description: "The users email."
-      },
-	  
-	  URL: {
-        type: String,
-		validate: {
-			validator: value => validator.isURL(value, { protocols: ['http','https'], require_tld: true, require_protocol: true 
-			}),
-			message: props => `${props.value} is not a valid Activity URL!`
-		},
-        required: [true, 'Activity URL is required.'],
-        description: "The activity url."
-      },
-    },
-    { versionKey: false }))   
+      { versionKey: false }
+    )
+  )
 };
