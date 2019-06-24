906191a1f7f721dc35e6274e88a0c9cf8e455633
Andrew Edwards <andrew.edwards@capgemini.com>
2019-06-24 21:47:53 +0100

    Added email reminder modal and used some logos.

--- public/js/activities.js
+++ public/js/activities.js
@@ -44,5 +44,52 @@ const showActivities = async () => {
   activities.data.forEach(addActivity);
 };
 
+const setLoginModel = async () => {
+  // Get the modal
+  var modal = document.getElementById("loginModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("loginBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  var emailText = document.getElementById("emailText");
+
+  var emailButton = document.getElementById("emailButton");
+
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+    emailButton.innerHTML="Send";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+    emailButton.innerHTML="Send";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      emailButton.innerHTML="Send";
+    }
+  }
+
+  emailButton.onclick = async () => {
+    var userEmail = emailText.value;
+
+    const email = await client.service('email-reminder').create({
+        "Email": userEmail,
+        "URL": "https://communityhub.azurewebsites.net/activities.html"
+      }); 
+
+    emailButton.innerHTML="Sent!";
+  }
+}
+
 showActivities();
 
+setLoginModel();
